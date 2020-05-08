cfbe74b61604542120c051f7a06a927c3bc5fbdd
minkyeng <minnn.xyz@gmail.com>
2020-05-08 22:33:06 +0900

    연습문제 6-1, 6-2

---
+++ src/ch06/Exercise6_2.java
@@ -0,0 +1,30 @@
+package ch06;
+
+public class Exercise6_2 {
+	public static void main(String[] args) {
+		SutdaCard card1 = new SutdaCard(3, false);
+		SutdaCard card2 = new SutdaCard();
+		
+		System.out.println(card1.info());
+		System.out.println(card2.info());
+	}
+}
+
+class SutdaCard{
+	int num;
+	boolean isKwang;
+	
+	public SutdaCard() {
+		this.num = 1;
+		this.isKwang = true;
+	}
+	
+	public SutdaCard(int num, boolean isKwang) {
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	
+	public String info() {
+		return isKwang?num+"K":num+"";
+	}
+}
