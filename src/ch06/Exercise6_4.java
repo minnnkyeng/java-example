58c32451857e5bd627612ed279cc6b13ce14021d
minkyeng <minnn.xyz@gmail.com>
2020-05-09 09:53:49 +0900

    연습문제 6-5(생성자,info())

--- src/ch06/Exercise6_4.java
+++ src/ch06/Exercise6_4.java
@@ -13,22 +13,44 @@ public class Exercise6_4 {
 		System.out.println("ÀÌ¸§:" + s.name);
 		System.out.println("ÃÑÁ¡:" + s.getTotal());
 		System.out.println("Æò±Õ:" + s.getAverage());
+
+		Student s1 = new Student("È«±æµ¿", 1, 1, 100, 60, 76);
+
+		System.out.println(s1.info());
 	}
 }
 
-class Student{
+class Student {
 	String name;
 	int ban;
 	int no;
 	int kor;
 	int eng;
 	int math;
-	
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name, int ban, int no, int kor, int eng, int math) {
+		this.name = name;
+		this.ban = ban;
+		this.no = no;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+
 	public int getTotal() {
-		return kor+eng+math;
+		return kor + eng + math;
 	}
-	
+
 	public float getAverage() {
-		return getTotal()/3.0f;
+		return getTotal() / 3.0f;
 	}
+
+	public String info() {
+		return name + "," + ban + "," + no + "," + kor + "," + eng + "," + math;
+	}
+
 }
